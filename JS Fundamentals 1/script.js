d1cd8389c9fd50bf9b07d9e0f85540fd59269902
Ahraz Arfi <ahrazarfi@gmail.com>
2021-02-15 21:47:47 +0530

    values and variables

--- JS Fundamentals 1/script.js
+++ JS Fundamentals 1/script.js
@@ -1,4 +1,51 @@
 let js = 'amazing';
-if(js==='amazing') alert('Javascript is FUN');
-40+8+23+78;
+// if(js==='amazing') alert('Javascript is FUN');
+// 40+8+23+78;
 console.log(40+8+23+78);
+
+console.log('Ahraz Arfi');
+console.log(69);
+
+//VALUES AND VARIABLES
+/* we can store values into variables using the let keyword
+this is called declaring a variable.
+suppose a variable is a box which can hold some things. We can write a label on the box to find it later or reuse it. Variable works exactly in the same way where the variable name is the label and the value is the thing according to the above analogy. */
+
+let firstName = "Ahraz";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//Conevntions and rules for naming variables
+/* 
+--->camelCase convention
+    example -> firstName, firstNameAndLastName.
+    - it is a convention to not start variable name with an UPPERCASE letter as that is used in OOP JS.
+    - variable names should be descriptive.
+---> Some hard rules for naming variables
+    - varible names can only contain letters, numbers, underscore _,  or dollar $ sign. Ex- ab&ad (illegal), $abc (legal).
+    - variable names should not be reserve keywords in JS. ex- new, function
+    - Variables cannot start with a number. Ex- 3abc (illegal variable  name)
+
+*/
+
+/* let 3ac = 4;
+let $3a&c = 4;
+let a.bc = 4;
+let function = 5; 
+
+ALL THE ABOVE ARE ILLEGAL VARIABLE NAMES.
+
+*/
+let a_bc = 8;
+let _abc = 4
+let $3ac = 4;
+
+let PI = 3.14; //uppercase for a constant
+
+let myFirstJob = 'freelancer'; //descriptive variable name
+let myCurrentJob = 'Marketer'; //descriptive variable name
+
+let job1 = 'freelancer';
+let job2 = 'marketer';

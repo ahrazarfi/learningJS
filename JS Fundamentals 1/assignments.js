64f53ebfb1dd1e7f74807e79538c1fb5822dc054
Ahraz Arfi <ahrazarfi@gmail.com>
2021-02-16 00:41:21 +0530

    adding assignments.js complete till data types

---
+++ JS Fundamentals 1/assignments.js
@@ -0,0 +1,16 @@
+//VALUES AND VARIABLES
+let country = 'India';
+let continent = 'Asia';
+let population = "1.36 billion";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);

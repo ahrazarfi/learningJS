3f7760827ca49d8382ecc312c72eb83786209b96
Ahraz Arfi <ahrazarfi@gmail.com>
2021-03-14 13:06:27 +0530

    add gitignore

--- JS Fundamentals 1/assignments.js
+++ JS Fundamentals 1/assignments.js
@@ -1,4 +1,4 @@
-//VALUES AND VARIABLES
+/* //VALUES AND VARIABLES
 let country = 'India';
 let continent = 'Asia';
 let population = "1.36 billion";
@@ -13,4 +13,18 @@ let language;
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
-console.log(typeof language);
+console.log(typeof language); */
+
+//let, const, and var
+const language = "Hindi"
+const country = "India";
+const continent = "Asia";
+let population = 1360;
+const isIsland = false;
+//isIsland = true; // gives error: assignment to constant variable
+
+//Basic operators
+const splitInHalf = population / 2;
+console.log(population+1);
+const popGreaterFinland = true;
+
